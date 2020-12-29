aaa841c2070927d8aac6aafd1b45e7ca75923948
Skity666 <Skity666@163.com>
2020-12-29 09:03:17 +0800

    初始化

---
+++ src/main/java/com/example/more_env/MoreEnvApplication.java
@@ -0,0 +1,13 @@
+package com.example.more_env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoreEnvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/more_env/MoreEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.more_env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreEnvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
